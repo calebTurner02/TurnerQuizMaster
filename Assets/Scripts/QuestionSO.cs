dff7d73f900c624fe9fea333c564fdc55e2b6d90
Caleb Turner <turner632@morrisville.edu>
2022-03-08 14:34:16 -0500

    Arrays

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -8,9 +8,20 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2, 6)]
    [SerializeField] string strQuestion = "Enter quiz question here";
+   [SerializeField] string[] strAnswer = new string[4];
+   [SerializeField] int intCorrectAnswerIndex;
 
    public string strgetQuestion()
    {
        return strQuestion;
+   }
+
+   public string strGetAnswer(int index)
+   {
+       return strAnswer[index];
+   }
+   public int intGetCorrectAnswerIndex()
+   {
+       return intCorrectAnswerIndex;
    }
 }
