ba20afab73db0c81c6f360e9ab7dd339ff63214a
Caleb Turner <turner632@morrisville.edu>
2022-03-08 15:07:30 -0500

    For Loops

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -7,8 +7,15 @@ public class Quiz : MonoBehaviour
 {
    [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
     void Start()
     {
         questionText.text = question.strgetQuestion();
+
+        for(int intI = 0; intI < answerButtons.Length; intI++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[intI].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.strGetAnswer(intI);
+        }     
     }  
 }
